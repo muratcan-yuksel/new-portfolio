8e68064249d0a713f103f6d806e2cddaa614aadc
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-28 03:15:39 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import Navbar from "./components/NavBarComp";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="mainBody">
+      <Navbar />
+      <Button variant="success">Success</Button>{" "}
     </div>
   );
-}
+};
 
 export default App;
