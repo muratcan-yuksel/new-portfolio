1c02531db88ebdacc8726d820af59979db7ad8a5
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-28 04:43:27 +0300

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,22 +12,34 @@ const Skills = () => {
         </thead>
         <tbody>
           <tr>
-            <td>@mdo</td>
+            <td>Vanilla Javascript/React.js/Vuejs</td>
           </tr>
           <tr>
-            <td>@mdo</td>
+            <td>Ruby/ Ruby on Rails</td>
           </tr>{" "}
           <tr>
-            <td>@mdo</td>
+            <td>-Solidity/hardhat</td>
           </tr>{" "}
           <tr>
-            <td>@mdo</td>
+            <td>Bootstrap/React-Bootstrap/Chakra UI</td>
           </tr>{" "}
           <tr>
-            <td>@mdo</td>
+            <td>Jest/React Testing Library</td>
           </tr>{" "}
           <tr>
-            <td>@mdo</td>
+            <td>MongoDB/Google Firebase/Cloud Firestore</td>
+          </tr>{" "}
+          <tr>
+            <td>Chartjs/Apexcharts</td>
+          </tr>{" "}
+          <tr>
+            <td>HTML/CSS</td>
+          </tr>{" "}
+          <tr>
+            <td>Webpack</td>
+          </tr>{" "}
+          <tr>
+            <td>Git</td>
           </tr>
         </tbody>
       </Table>
