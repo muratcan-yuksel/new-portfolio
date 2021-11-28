5cea606e54ce139af686fea4cd43b225d0fd02fb
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-28 03:35:54 +0300

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import "./App.css";
 import Navbar from "./components/NavBarComp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div id="mainBody">
       <Navbar />
-      <Button variant="success">Success</Button>{" "}
+      <Header />
     </div>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/header.css";
+
+const Header = () => {
+  return (
+    <div class="container">
+      <h1 class="neon">Hey! This is Murat. I'm a web developer.</h1>
+    </div>
+  );
+};
+
+export default Header;
