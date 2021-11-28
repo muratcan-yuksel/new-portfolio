f55d76b853026a15dcbfcfab78751b86f0a0775e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-28 04:29:56 +0300

    figured bootstrap out

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import Button from "react-bootstrap/Button";
 import "./App.css";
 import Navbar from "./components/NavBarComp";
 import Header from "./components/Header";
+import Skills from "./components/Skills";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const App = () => {
   return (
     <div id="mainBody">
       <Navbar />
-      <Header />
+
+      <Container fluid>
+        <Row>
+          <Col lg={8} xs={12} sm={12} md={8} xxl={8}>
+            {" "}
+            <Header />
+          </Col>
+          <Col lg={4} xs={12} sm={12} md={4} xxl={4}>
+            {" "}
+            <div class="techStack">
+              <Skills />
+            </div>
+          </Col>
+        </Row>{" "}
+      </Container>
     </div>
   );
 };
---
+++ src/components/Skills.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Table from "react-bootstrap/Table";
+
+const Skills = () => {
+  return (
+    <div>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Tech stack</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>@mdo</td>
+          </tr>{" "}
+          <tr>
+            <td>@mdo</td>
+          </tr>{" "}
+          <tr>
+            <td>@mdo</td>
+          </tr>{" "}
+          <tr>
+            <td>@mdo</td>
+          </tr>{" "}
+          <tr>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Skills;
